64c1dd006f0cbec873657fd21b0208871ea74134
vanessa-dia <tvh.mai.us@gmail.com>
2020-09-26 11:37:50 +0700

    init project

---
+++ Test Suites/New Test Suite.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>New Test Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>a626ced0-2aef-4a81-9283-ad6a1b9a71b9</testSuiteGuid>
+   <testCaseLink>
+      <guid>adca6e90-a2c5-4930-9dff-b7c9b74dec45</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/UnitTest</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
